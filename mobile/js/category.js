9ad37859b938fd564f895c8f4f6aca542484e5ae
DuHuaL <469580396@qq.com>
2020-09-28 03:55:57 +0800

    切换分类并渲染对应的二级分类

--- mobile/js/category.js
+++ mobile/js/category.js
@@ -11,10 +11,14 @@ var Category = function() {
 //初始化方法
 Category.prototype.init = function() {
   var that = this;
+  //一级分类
   that.renderTopCategory(function(data) {
     var id= data.rows[0].id
+    //二级分类
     that.renderSecondCategory(id);
   });
+  //初始化切换功能
+  that.toggleTopCategory();
 };
 //渲染顶级分类
 Category.prototype.renderTopCategory = function(callback) {
@@ -35,7 +39,6 @@ Category.prototype.renderTopCategory = function(callback) {
 };
 //渲染二级分类
 Category.prototype.renderSecondCategory = function(id) {
-  console.log(id);
   $.ajax({
     type: 'get',
     url: 'http://localhost:3000/category/querySecondCategory',
@@ -50,4 +53,15 @@ Category.prototype.renderSecondCategory = function(id) {
   });
 };
 //切换分类
-Category.prototype.toggleTopCategory = function() {};
+Category.prototype.toggleTopCategory = function() {
+  var that = this;
+  // tap  zepto touch mui 都会提供
+  $('.lt_cateLeft ul').on('tap','li a', function() {
+    // 修改样式
+    $('.lt_cateLeft ul li').removeClass('now');
+    $(this).parent().addClass('now');
+    //二级分类渲染
+    var id = $(this).data('id');
+    that.renderSecondCategory(id);
+  });
+};
